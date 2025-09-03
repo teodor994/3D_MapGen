26bd8c5e86f3de4bf02ca0548aaf7e20b2337b74
Teodor Andrei <teodorvica@gmail.com>
2025-09-03 04:41:37 +0300

    Added an Generate button and an Auto Save button to generate automatically a new map on every change of the parameters

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
-[CustomEditor (typeof (MapGenerator))]
+[CustomEditor (typeof (MapGenerator))] // Here we modify the default Editor to add a button
+// Generate button
 public class MapGeneratorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         MapGenerator mapGen = (MapGenerator)target;
 
-        DrawDefaultInspector();
+        if (DrawDefaultInspector()) //If any value in editor was changed - width or height
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap(); //regenerate automatically the map
+            }
+        }
 
         if(GUILayout.Button("Generate"))
         {
